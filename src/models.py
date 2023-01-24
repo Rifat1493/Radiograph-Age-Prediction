3cf87f171277e1e4dbb2acb88be321201b633b68
mrifat <mrifat@t25.torre.math.unipd.it>
2023-01-24 18:29:37 +0100

    model file changed

--- src/models.py
+++ src/models.py
@@ -992,184 +992,7 @@ class Inception:
 
         return X
 
-
-class Unet:
-    @staticmethod
-    def unet():
-        inputs = Input(shape=(hparams.IMG_SIZE, hparams.IMG_SIZE, 3))
-        # Contraction path
-        c1 = Conv2D(
-            16,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(inputs)
-        c1 = Dropout(0.1)(c1)
-        c1 = Conv2D(
-            16,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c1)
-        p1 = MaxPooling2D((2, 2))(c1)
-
-        c2 = Conv2D(
-            32,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(p1)
-        c2 = Dropout(0.1)(c2)
-        c2 = Conv2D(
-            32,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c2)
-        p2 = MaxPooling2D((2, 2))(c2)
-
-        c3 = Conv2D(
-            64,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(p2)
-        c3 = Dropout(0.2)(c3)
-        c3 = Conv2D(
-            64,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c3)
-        p3 = MaxPooling2D((2, 2))(c3)
-
-        c4 = Conv2D(
-            128,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(p3)
-        c4 = Dropout(0.2)(c4)
-        c4 = Conv2D(
-            128,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c4)
-        p4 = MaxPooling2D(pool_size=(2, 2))(c4)
-
-        c5 = Conv2D(
-            256,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(p4)
-        c5 = Dropout(0.3)(c5)
-        c5 = Conv2D(
-            256,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c5)
-
-        # Expansive path
-        u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same")(c5)
-        u6 = concatenate([u6, c4])
-        c6 = Conv2D(
-            128,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(u6)
-        c6 = Dropout(0.2)(c6)
-        c6 = Conv2D(
-            128,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c6)
-
-        u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same")(c6)
-        u7 = concatenate([u7, c3])
-        c7 = Conv2D(
-            64,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(u7)
-        c7 = Dropout(0.2)(c7)
-        c7 = Conv2D(
-            64,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c7)
-
-        u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding="same")(c7)
-        u8 = concatenate([u8, c2])
-        c8 = Conv2D(
-            32,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(u8)
-        c8 = Dropout(0.1)(c8)
-        c8 = Conv2D(
-            32,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c8)
-
-        u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding="same")(c8)
-        u9 = concatenate([u9, c1], axis=3)
-        c9 = Conv2D(
-            16,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(u9)
-        c9 = Dropout(0.1)(c9)
-        c9 = Conv2D(
-            16,
-            (3, 3),
-            activation=hparams.HID_ACT_FUNC,
-            kernel_initializer="he_normal",
-            padding="same",
-        )(c9)
-
-        d1 = Flatten()(c9)
-        d2 = Dense(16, activation=hparams.HID_ACT_FUNC)(d1)
-        if hparams.GENDER:
-            gd_input = Input(shape=(1,))
-            gd_d1 = Dense(16, activation=hparams.HID_ACT_FUNC)(gd_input)
-            d1_d2 = concatenate([d2, gd_d1])
-
-            out = Dense(1, activation="linear")(d1_d2)
-            model = Model(inputs=[inputs, gd_input], outputs=[out])
-        else:
-            out = Dense(1, activation="linear")(d2)
-            model = Model(inputs=[inputs], outputs=[out])
-
-        return model
-
+    
 
 class ResidualAttentionUnet:
     @staticmethod
@@ -1501,4 +1324,198 @@ class CnnAttentionUnet:
         else:
             out = Dense(1, activation="linear")(d2)
             model = Model(inputs=[inputs], outputs=[out])
+        return model
+
+
+class Unet:
+    @staticmethod
+    def unet():
+        inputs = Input(shape=(hparams.IMG_SIZE, hparams.IMG_SIZE, 3))
+        # Contraction path
+        c1 = Conv2D(
+            16,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(inputs)
+        c1 = BatchNormalization()(c1)
+        c1 = Dropout(0.1)(c1)
+        c1 = Conv2D(
+            16,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c1)
+        c1 = BatchNormalization()(c1)
+        p1 = MaxPooling2D((2, 2))(c1)
+
+        c2 = Conv2D(
+            32,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(p1)
+        c2 = BatchNormalization()(c2)
+        c2 = Dropout(0.1)(c2)
+        c2 = Conv2D(
+            32,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c2)
+        c2 = BatchNormalization()(c2)
+        p2 = MaxPooling2D((2, 2))(c2)
+
+        c3 = Conv2D(
+            64,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(p2)
+        c3 = BatchNormalization()(c3)
+        c3 = Dropout(0.2)(c3)
+        c3 = Conv2D(
+            64,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c3)
+
+        ## add batch
+        c3 = BatchNormalization()(c3)
+
+        p3 = MaxPooling2D((2, 2))(c3)
+
+        c4 = Conv2D(
+            128,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(p3)
+        c4 = BatchNormalization()(c4)
+        c4 = Dropout(0.2)(c4)
+        c4 = Conv2D(
+            128,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c4)
+        c4 = BatchNormalization()(c4)
+        p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+        c5 = Conv2D(
+            256,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(p4)
+        c5 = BatchNormalization()(c5)
+        c5 = Dropout(0.3)(c5)
+        c5 = Conv2D(
+            256,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c5)
+        c5 = BatchNormalization()(c5)
+        # Expansive path
+        u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same")(c5)
+        u6 = concatenate([u6, c4])
+        c6 = Conv2D(
+            128,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(u6)
+        c6 = BatchNormalization()(c6)
+        c6 = Dropout(0.2)(c6)
+        c6 = Conv2D(
+            128,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c6)
+        c6 = BatchNormalization()(c6)
+        u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same")(c6)
+        u7 = concatenate([u7, c3])
+        c7 = Conv2D(
+            64,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(u7)
+        c7 = BatchNormalization()(c7)
+        c7 = Dropout(0.2)(c7)
+        c7 = Conv2D(
+            64,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c7)
+        c7 = BatchNormalization()(c7)
+        u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding="same")(c7)
+        u8 = concatenate([u8, c2])
+        c8 = Conv2D(
+            32,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(u8)
+        c8 = BatchNormalization()(c8)
+        c8 = Dropout(0.1)(c8)
+        c8 = Conv2D(
+            32,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c8)
+        c8 = BatchNormalization()(c8)
+        u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding="same")(c8)
+        u9 = concatenate([u9, c1], axis=3)
+        c9 = Conv2D(
+            16,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(u9)
+        c9 = BatchNormalization()(c9)
+        c9 = Dropout(0.1)(c9)
+        c9 = Conv2D(
+            16,
+            (3, 3),
+            activation=hparams.HID_ACT_FUNC,
+            kernel_initializer="he_normal",
+            padding="same",
+        )(c9)
+        c9 = BatchNormalization()(c9)
+        d1 = Flatten()(c9)
+        d2 = Dense(16, activation=hparams.HID_ACT_FUNC)(d1)
+        if hparams.GENDER:
+            gd_input = Input(shape=(1,))
+            gd_d1 = Dense(16, activation=hparams.HID_ACT_FUNC)(gd_input)
+            d1_d2 = concatenate([d2, gd_d1])
+
+            out = Dense(1, activation="linear")(d1_d2)
+            model = Model(inputs=[inputs, gd_input], outputs=[out])
+        else:
+            out = Dense(1, activation="linear")(d2)
+            model = Model(inputs=[inputs], outputs=[out])
+
         return model
